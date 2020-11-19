461b7399c535532a53f87192480b516629452dba
Cherise Knox <cheriseknox@gmail.com>
2020-11-19 10:45:22 -0500

    deleted unnecessary comments within App file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-//import pages
 import Home from './pages/Home';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
 import Error from './pages/Error';
-
-//import componenets
 import Navbar from './components/Navbar';
 
 const App = () => {
